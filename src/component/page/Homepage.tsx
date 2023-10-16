1a5db6db16110b3105de0df1388c4d458fecf27f
skovacevic7 <sinisa.kovacevic@fina.hr>
2023-10-16 14:48:31 +0200

    Rad oko responzivnosti stranice

--- src/component/page/Homepage.tsx
+++ src/component/page/Homepage.tsx
@@ -5,7 +5,7 @@ export const Homepage: React.FC = () => {
     <>
       <div className="homepage_main">
         <div className="homepage_title">
-          <h1>Welcome to info page</h1>
+          <h1>WELCOME TO INFO PAGE</h1>
         </div>
         <div className="homepage_image_list">
           <HomeImageList/>
